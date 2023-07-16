00b675d80a6ff5303b9953cf71ee665dd0cf9069
Rajkumar-justcoder <meet.r.graphicat10@gmail.com>
2023-07-16 11:45:12 +0530

    Fix: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     format: ['esm', 'cjs'],
     target: ['es2022'],
+    bundle: false,
     treeshake: true,
+    entry: ["src/**/*.ts"],
     dts: true,
     clean: true,
-    outDir:'lib'
+    outDir: 'lib'
 });
